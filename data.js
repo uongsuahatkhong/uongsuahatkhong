5c9da8b930a796fee08058c9a70109cf6e724bad
uongsuahatkhong <motvitinhtu07@gmail.com>
2024-11-06 15:22:42 +0700

    Update data.js

--- data.js
+++ data.js
@@ -7,21 +7,21 @@ let songs = [
     },
     {
         name: 'Nghệ thuật là liều thuốc chữa lành',
-        path: '../Filemusic/2Sayyestoheaven.mp3',
+        path: 'Filemusic/2Sayyestoheaven.mp3',
         artist: 'artist 2',
-        cover: '../Fileanhchogiaodien/yestoheaven.png'
+        cover: 'Fileanhchogiaodien/yestoheaven.png'
     },
     {
         name: 'Tình yêu với nghệ thuật',
-        path: '../Filemusic/3LoveStory.mp3',
+        path: 'Filemusic/3LoveStory.mp3',
         artist: 'artist 3',
-        cover: '../Fileanhchogiaodien/Love Story.png'
+        cover: 'Fileanhchogiaodien/Love Story.png'
     },
     {
         name: 'Rain,books and coffee',
-        path: '../Filemusic/Nhac 4.mp3',
+        path: 'Filemusic/Nhac 4.mp3',
         artist: 'artist 4',
-        cover: '../Fileanhchogiaodien/Rain.png'
+        cover: 'Fileanhchogiaodien/Rain.png'
     },
 
 ]
